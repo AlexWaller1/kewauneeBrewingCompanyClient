945e1adfbf6c1a76eba07f17e33535a4dafafd1e
Alex Waller <alex@artofalexwaller.com>
2022-04-26 21:36:16 -0400

    BrowserRouter

--- NewReactApp-6/src/App.js
+++ NewReactApp-6/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import RouterLinks from "./components/RouterLinks";
 
 function App() {
   return (
     <div className="App">
+      <RouterLinks />
       <Header text="Kewaunee Brewing Company" />
     </div>
   );
---
+++ NewReactApp-6/src/components/RouterLinks.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import HomePage from './HomeComp/HomePage';
+import MenuPage from './MenuComp/MenuPage';
+import ReviewsPage from './ReviewsComp/ReviewsPage';
+import WeatherPage from './WeatherComp/WeatherPage';
+import AboutPage from './AboutComp/AboutPage';
+import ContactPage from './ContactComp/ContactPage';
+
+
+ const RouterLinks = () => {
+  return (
+    <Router >
+     <Routes>
+       <Route  path="/" element={HomePage}/>
+       <Route  path="/menu" element={MenuPage}/>
+       <Route  path="/reviews" element={ReviewsPage}/>
+       <Route  path="/weather" element={WeatherPage}/>
+       <Route  path="/about" element={AboutPage}/>
+       <Route  path="/contact" element={ContactPage}/>
+     </Routes>
+     </Router>
+  )
+}
+
+export default RouterLinks;
