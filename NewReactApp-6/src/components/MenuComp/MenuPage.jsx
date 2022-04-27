5c8fdc20e9b8866cb61ebe6833204347898a8124
Alex Waller <alex@artofalexwaller.com>
2022-04-27 01:04:58 -0400

    Building Menu Section

--- NewReactApp-6/src/components/MenuComp/MenuPage.jsx
+++ NewReactApp-6/src/components/MenuComp/MenuPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import MenuPageHeader from './MenuPageHeader'
+import MenuListDiv from './MenuListDiv';
+import MenuParagraphDiv from './MenuParagraphDiv';
 import { useState, useEffect } from "react";
 
  const MenuPage = () => {
   return (
     <div className='menu-page-div'>
         <MenuPageHeader text="Take A Look At Our Menu"/>
+        <MenuParagraphDiv />
+        <MenuListDiv />
+
     </div>
   )
 }
---
+++ NewReactApp-6/src/components/MenuComp/MenuParagraphDiv.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const MenuParagraphDiv = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default MenuParagraphDiv
