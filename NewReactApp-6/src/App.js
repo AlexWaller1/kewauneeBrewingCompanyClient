a788e767033fe0b69b38bbadd76fcd9d24412665
Alex Waller <alex@artofalexwaller.com>
2022-04-28 00:06:30 -0400

    adding cart page

--- NewReactApp-6/src/App.js
+++ NewReactApp-6/src/App.js
@@ -12,6 +12,114 @@ function App() {
 
   const [cartCount, setCartCount] = useState({count: 0});
 
+  const [appetizers, setAppetizers] = useState([
+    {
+        id: 1,
+        name: "Buffalo Wings",
+        img: "",
+        price: 14.99
+    },
+    {
+        id: 2,
+        name: "Ultimate Stacked Nachos",
+        img: "",
+        price: 12.99
+    },
+    {
+        id: 3,
+        name: "Barria Tacos",
+        img: "",
+        price: 16.99
+    },
+    {
+        id: 4,
+        name: "Potstickers",
+        img: "",
+        price: 17.99
+    }
+])
+
+const [mainCourses, setFoodMenu] = useState([
+    {
+        id: 1,
+        name: "Cheese Burger and Fries",
+        img: "",
+        price: 15.99
+    },
+    {
+        id: 2, 
+        name: "Ultimate Turkey Club",
+        img: "",
+        price: 12.99
+    },
+    {
+        id: 3,
+        name: "Cowboy Ribeye",
+        img: "",
+        price: 34.99
+    },
+    {
+        id: 4,
+        name: "KBC Spare Ribs",
+        img: "",
+        price: 24.99
+    }
+]);
+
+const [desserts, setDesserts] = useState([
+    {
+        id: 1,
+        name: "Vanilla Sundae",
+        img: "",
+        price: 9.99
+    },
+    {
+        id: 2,
+        name: "Tiramasu",
+        img: "",
+        price: 14.99
+    },
+    {
+        id: 3,
+        name: "Chocolate Mousse Cake",
+        img: "",
+        price: 13.99
+    },
+    {
+        id: 4,
+        name: "Vanilla Lemon Cake",
+        img: "",
+        price: 12.99
+    }
+])
+
+const addToCart = (id) => {
+    console.log("Appetizer Added to Cart", "new", id);
+
+    let newAppetizer = appetizers.filter(a1 => a1.id == id);
+
+    setCart([...userCart, newAppetizer]);
+}
+
+const addToCart2 = (id) => {
+    console.log("Main Course Added to Cart", "new", id);
+
+    let newMainCourse = mainCourses.filter(m1 => m1.id == id);
+
+    setCart([...userCart, newMainCourse]);
+}
+
+const addToCart3 = (id) => {
+    console.log("Dessert Added to Cart", "new", id);
+
+    let newDessert = desserts.filter(d1 => d1.id == id);
+
+    setCart([...userCart, newDessert]);
+
+}
+
+
+
   return (
     <Router>
       <div className="App">
@@ -21,7 +129,7 @@ function App() {
       </div>
       <NavLinks />
 
-      <RouterLinks userCart={userCart} cartCount={cartCount}/>
+      <RouterLinks userCart={userCart} cartCount={cartCount} appetizers={appetizers} mainCourses={mainCourses} desserts={desserts} addToCart={addToCart} addToCart2={addToCart2} addToCart3={addToCart3}/>
       
     </Router>
   );
--- NewReactApp-6/src/components/RouterLinks.jsx
+++ NewReactApp-6/src/components/RouterLinks.jsx
@@ -6,18 +6,20 @@ import ReviewsPage from './ReviewsComp/ReviewsPage';
 import WeatherPage from './WeatherComp/WeatherPage';
 import AboutPage from './AboutComp/AboutPage';
 import ContactPage from './ContactComp/ContactPage';
+import CartPage from './CartComp/CartPage';
 
 
- const RouterLinks = ({ userCart, cartCount}) => {
+ const RouterLinks = ({ userCart, cartCount, appetizers, mainCourses, desserts, addToCart, addToCart2, addToCart3}) => {
   return (
     
      <Routes>
        <Route  path="/" element={<HomePage userCart={userCart} cartCount={cartCount}/>}/>
-       <Route  path="/menu" element={<MenuPage userCart={userCart} cartCount={cartCount}/>}/>
+       <Route  path="/menu" element={<MenuPage userCart={userCart} cartCount={cartCount} appetizers={appetizers} mainCourses={mainCourses} desserts={desserts} addToCart={addToCart} addToCart2={addToCart2} addToCart3={addToCart3}/>}/>
        <Route  path="/reviews" element={<ReviewsPage userCart={userCart} cartCount={cartCount}/>}/>
        <Route  path="/weather" element={<WeatherPage userCart={userCart} cartCount={cartCount}/>}/>
        <Route  path="/about" element={<AboutPage userCart={userCart} cartCount={cartCount}/>}/>
        <Route  path="/contact" element={<ContactPage userCart={userCart} cartCount={cartCount}/>}/>
+       <Route  path="/cart" element={<CartPage userCart={userCart} cartCount={cartCount}/>}/>
      </Routes>
     
   )
