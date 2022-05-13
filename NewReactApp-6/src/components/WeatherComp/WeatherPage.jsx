52855ce291383a09c5c6c41aacc842529041fb80
Alex Waller <alex@artofalexwaller.com>
2022-05-12 22:13:35 -0400

    weather list css

--- NewReactApp-6/src/components/WeatherComp/WeatherPage.jsx
+++ NewReactApp-6/src/components/WeatherComp/WeatherPage.jsx
@@ -17,14 +17,38 @@ import { useState, useEffect} from "react"
          {weather["list"] != undefined ? <div>
           <h2>{`${weather["city"].name} Weather For Next Seven Days`}</h2>
          <br />
-         <ul id="weather-info-list">
-          
+         <ul id="weather-info-list" >
+          <li className="weather-list-item">
            <div id="weather-list-div-1">
              <h4>{weather["list"][0]["dt_txt"]}</h4>
              <img src={`http://openweathermap.org/img/wn/${weather["list"][0]["weather"][0]["icon"]}@4x.png`} alt="weather image" />
-           
-         
-
+             <h4>{`${Math.round(weather["list"][0]["main"].temp)}°F`}</h4>
+             <h4>{weather["list"][0]["weather"][0].description}</h4>
+           </div>
+           </li>
+           <div id="weather-list-div-2">
+             <h4>{weather["list"][8]["dt_txt"]}</h4>
+             <img src={`http://openweathermap.org/img/wn/${weather["list"][8]["weather"][0]["icon"]}@4x.png`} alt="weather image" />
+             <h4>{`${Math.round(weather["list"][8]["main"].temp)}°F`}</h4>
+             <h4>{weather["list"][8]["weather"][0].description}</h4>
+           </div>
+           <div id="weather-list-div-3">
+             <h4>{weather["list"][16]["dt_txt"]}</h4>
+             <img src={`http://openweathermap.org/img/wn/${weather["list"][16]["weather"][0]["icon"]}@4x.png`} alt="weather image" />
+             <h4>{`${Math.round(weather["list"][16]["main"].temp)}°F`}</h4>
+             <h4>{weather["list"][16]["weather"][0].description}</h4>
+           </div>
+           <div id="weather-list-div-4">
+             <h4>{weather["list"][24]["dt_txt"]}</h4>
+             <img src={`http://openweathermap.org/img/wn/${weather["list"][24]["weather"][0]["icon"]}@4x.png`} alt="weather image" />
+             <h4>{`${Math.round(weather["list"][24]["main"].temp)}°F`}</h4>
+             <h4>{weather["list"][24]["weather"][0].description}</h4>
+           </div>
+           <div id="weather-list-div-5">
+             <h4>{weather["list"][32]["dt_txt"]}</h4>
+             <img src={`http://openweathermap.org/img/wn/${weather["list"][32]["weather"][0]["icon"]}@4x.png`} alt="weather image" />
+             <h4>{`${Math.round(weather["list"][32]["main"].temp)}°F`}</h4>
+             <h4>{weather["list"][32]["weather"][0].description}</h4>
            </div>
          </ul>
 
