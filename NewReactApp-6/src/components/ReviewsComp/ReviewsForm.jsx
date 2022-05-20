a3d4899c70c8a65c29232f847876c8fae189d45d
Alex Waller <alex@artofalexwaller.com>
2022-05-20 19:05:50 -0400

    setup proxy server for client

---
+++ NewReactApp-6/src/components/ReviewsComp/ReviewsForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ReviewsForm = () => {
+
+    state = {
+        title: '',
+        review: ''
+    }
+  return (
+    <div>ReviewsForm</div>
+  )
+}
+
+export default ReviewsForm;
