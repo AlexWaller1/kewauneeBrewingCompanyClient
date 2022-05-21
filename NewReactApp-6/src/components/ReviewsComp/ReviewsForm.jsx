7f8363937c877007eb790a0a9b1d10e655d75265
Alex Waller <alex@artofalexwaller.com>
2022-05-21 14:27:03 -0400

    reviewForm

--- NewReactApp-6/src/components/ReviewsComp/ReviewsForm.jsx
+++ NewReactApp-6/src/components/ReviewsComp/ReviewsForm.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useState } from "react";
 
 const ReviewsForm = () => {
 
-    state = {
-        title: '',
-        review: ''
-    }
+   const [revForm, setRevForm] = useState({
+     title: "",
+     text: ""
+   })
+
   return (
-    <div>ReviewsForm</div>
+    <div id="review form">
+      <div className="form-control">
+        <label>Post Title</label>
+        <input type="text" placeholder='Post Title...'/>
+      </div>
+      <div className="form-control">
+        <label>Leave Review Here</label>
+        <input type="textbox" placeholder="Type Review Here..."/>
+      </div>
+      <div className="form-control">
+        <input type="submit" className='btn btn-primary' value="submit review"/>
+      </div>
+    </div>
   )
 }
 
