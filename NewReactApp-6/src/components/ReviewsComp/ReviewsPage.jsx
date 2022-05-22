9fdb657d068cf02640dc73bd9d6546d3d04b6cea
Alex Waller <alex@artofalexwaller.com>
2022-05-21 22:02:36 -0400

    connected to server

--- NewReactApp-6/src/components/ReviewsComp/ReviewsPage.jsx
+++ NewReactApp-6/src/components/ReviewsComp/ReviewsPage.jsx
@@ -8,7 +8,13 @@ import ReviewsList from './ReviewsList'
 
   const [reviews, setReviews] = useState([]);
 
-  
+  useEffect(() => {
+    fetch("http://localhost:3006/api/comments").then(
+      res => res.json().then(
+        data => setReviews([...data])
+      )
+    )
+  })
   return (
     <div className='reviews-page-div'>
         <ReviewsPageHeader text="We Want To Hear About Your Experience!"/>
